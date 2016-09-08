e603fa293b12c2c9efb49ffc55ad147942a9e922
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-08 13:36:16 +0200

    Nicer responsiveness

--- templates/lix-metabox.php
+++ templates/lix-metabox.php
@@ -1,16 +1,19 @@
 <div id="lix-calculator-content">
-    <div class="row">
-        <div class="col">
-            <label><?php _e('Lix value', 'lix-calculator'); ?></label>
-            <em class="value lix"><?php _e('n/a', 'lix-calculator'); ?></em><br>
-            <span class="value readability"><?php _e('n/a', 'lix-calculator'); ?></span>
-        </div>
+    <div class="col">
+        <label>
+            <?php _e('Readability', 'lix-calculator'); ?>
+            <small><?php _e('Lix value', 'lix-calculator'); ?></small>
+        </label>
+        <em class="value lix"><?php _e('n/a', 'lix-calculator'); ?></em><br>
+        <span class="value readability"><?php _e('n/a', 'lix-calculator'); ?></span>
     </div>
-    <div class="row">
-        <div class="col">
-            <label><?php _e('Paragrah ratio', 'lix-calculator'); ?> <small><?php _e('sentences', 'lix-calculator'); ?>/<?php _e('paragraphs', 'lix-calculator'); ?></small></label>
-            <em class="value parahraph-ratio"><?php _e('n/a', 'lix-calculator'); ?></em><br>
-            <span class="value paragraph-ratio-rating"><?php _e('n/a', 'lix-calculator'); ?></span>
-        </div>
+
+    <div class="col">
+        <label>
+            <?php _e('Paragrah ratio', 'lix-calculator'); ?>
+            <small><?php _e('sentences', 'lix-calculator'); ?>/<?php _e('paragraphs', 'lix-calculator'); ?></small>
+        </label>
+        <em class="value parahraph-ratio"><?php _e('n/a', 'lix-calculator'); ?></em><br>
+        <span class="value paragraph-ratio-rating"><?php _e('n/a', 'lix-calculator'); ?></span>
     </div>
 </div>
