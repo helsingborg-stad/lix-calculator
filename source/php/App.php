f297e131886777f894c71b47d2834187cd9b3ccf
NiclasNorin <niclasn@outlook.com>
2024-03-01 11:39:42 +0100

    feat: webpack

--- source/php/App.php
+++ source/php/App.php
@@ -4,10 +4,83 @@ namespace LixCalculator;
 
 class App
 {
+    private $cacheBust;
+
     public function __construct()
     {
-        add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+        // add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+        add_action('wp_enqueue_scripts', array($this, 'enqueueScripts'));
         add_action('add_meta_boxes', array($this, 'addMetaBox'));
+
+        $this->cacheBust = new \LixCalculator\Helper\CacheBust();
+    }
+
+    public function enqueueScripts() 
+    {
+        wp_register_style(
+            'lix-calculator-css',
+            LIXCALCULATOR_URL . '/dist/' .
+            $this->cacheBust->name('css/lix-calculator.css')
+        );
+
+        wp_enqueue_style('lix-calculator-css');
+
+        wp_register_script(
+            'lix-calculator-js',
+            LIXCALCULATOR_URL . '/dist/' .
+            $this->cacheBust->name('js/lix-calculator.js'),
+            array(), '1.0.0', true
+        );
+
+        wp_localize_script('lix-calculator-js', 'LixCalculatorLang', $this->getJsLang());
+        wp_enqueue_script('lix-calculator-js');
+    }
+
+    private function getJsLang() 
+    {
+        return array(
+            'lix' => array(
+                'title' => __('Readability', 'lix-calculator'),
+                'description' => __('Lix value (lower is better)', 'lix-calculator'),
+                'very_hard' => __('Very hard', 'lix-calculator'),
+                'hard' => __('Hard', 'lix-calculator'),
+                'moderate' => __('Moderate', 'lix-calculator'),
+                'easy' => __('Easy', 'lix-calculator'),
+                'very_easy' => __('Very easy', 'lix-calculator'),
+            ),
+
+            'paragraph' => array(
+                'title' => __('Paragrah ratio', 'lix-calculator'),
+                'description' => __('Sentences per paragraph', 'lix-calculator'),
+                'good' => __('Good', 'lix-calculator'),
+                'low' => __('Low', 'lix-calculator'),
+                'high' => __('High', 'lix-calculator')
+            ),
+
+            'headline' => array(
+                'title' => __('Headline ratio', 'lix-calculator'),
+                'description' => __('Paragraphs per headline', 'lix-calculator')
+            ),
+
+            'moretag' => array(
+                'title' => __('Has more-tag', 'lix-calculator'),
+                'description' => __('Yes or no', 'lix-calculator'),
+                'missing' => __('Missing more-tag', 'lix-calculator'),
+                'has' => __('Has more-tag', 'lix-calculator')
+            ),
+
+            'total' => array(
+                'title' => __('Total', 'lix-calculator'),
+                'description' => __('Total score', 'lix-calculator'),
+                'bad' => __('Bad', 'lix-calculator'),
+                'ok' => __('Ok', 'lix-calculator'),
+                'good' => __('Good', 'lix-calculator')
+            ),
+
+            'yes' => __('Yes'),
+            'no' => __('No'),
+            'na' => __('n/a', 'lix-calculator')
+        );
     }
 
     public function enqueue()
